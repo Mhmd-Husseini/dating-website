fad1b94065ce4bcf4e3e5b986f3399062534a7d3
Husseini <mhmd.husseinyy@gmail.com>
2023-03-17 18:54:57 +0200

    migrating tables in db

---
+++ database/migrations/2023_03_17_152502_create_user_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_users', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id_action');
+            $table->integer('user2_id');
+            $table->boolean('favorite_block');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_users');
+    }
+};
