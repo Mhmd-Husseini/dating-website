1463726d9b6d00879aa095620b2e7a05abbc1e7f
Husseini <mhmd.husseinyy@gmail.com>
2023-03-18 17:05:53 +0200

    creating usercontroller to search users

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index(Request $request)
+    {
+        $currentUser = Auth::user();
+        $gender = $currentUser->gender == 'male' ? 'female' : 'male';
+
+        $query = User::where('gender', $gender)
+            ->where('id', '<>', $currentUser->id)
+            ->orderBy('created_at', 'desc');
+
+        if ($request->has('min_age')) {
+            $query->where('age', '>=', $request->input('min_age'));
+        }
+
+        if ($request->has('max_age')) {
+            $query->where('age', '<=', $request->input('max_age'));
+        }
+
+        if ($request->has('location')) {
+            $query->where('location', $request->input('location'));
+        }
+
+        if ($request->has('search')) {
+            $query->where('name', 'like', '%' . $request->input('search') . '%');
+        }
+
+        $users = $query->get();
+
+        return response()->json(['users' => $users]);
+    }
+}
